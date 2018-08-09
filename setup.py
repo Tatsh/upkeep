ec8f241b085f51e471fca73100e23ac16f0ac992
Andrew Udvare <audvare@gmail.com>
2018-08-09 17:25:18 -0400

    Fix content type

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 
 setup(
     name='upkeep',
-    version='1.0.0',
+    version='1.0.1',
     author='Andrew Udvare',
     author_email='audvare@gmail.com',
     url='https://github.com/Tatsh/upkeep',
     license='LICENSE.txt',
     description='Portage update helper scripts.',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     py_modules=['upkeep'],
     entry_points={
         'console_scripts': [
