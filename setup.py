195557d7358a5664ac32d67ae5c1bd5eba3c97c7
Andrew Udvare <audvare@gmail.com>
2019-04-28 13:02:57 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='upkeep',
-    version='1.2.1',
+    version='1.2.2',
     author='Andrew Udvare',
     author_email='audvare@gmail.com',
     url='https://github.com/Tatsh/upkeep',
