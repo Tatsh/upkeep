01d5a5b7c004c423c113797012a017d928bd3aa8
Andrew Udvare <audvare@gmail.com>
2020-03-11 01:58:48 -0400

    Add tests_requires

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 """Module with Portage helper commands."""
 from setuptools import setup
 
-setup(
-    name='upkeep',
-    version='1.2.7',
-    author='Andrew Udvare',
-    author_email='audvare@gmail.com',
-    url='https://github.com/Tatsh/upkeep',
-    license='LICENSE.txt',
-    description='Portage update helper scripts.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    py_modules=('upkeep', ),
-    entry_points={
-        'console_scripts': (
-            'ecleans = upkeep:ecleans',
-            'emerges = upkeep:emerges',
-            'esync = upkeep:esync',
-            'rebuild-kernel = upkeep:rebuild_kernel_command',
-            'upgrade-kernel = upkeep:upgrade_kernel_command',
-        ),
-    },
-)
+setup(name='upkeep',
+      version='1.2.7',
+      author='Andrew Udvare',
+      author_email='audvare@gmail.com',
+      url='https://github.com/Tatsh/upkeep',
+      license='LICENSE.txt',
+      description='Portage update helper scripts.',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      py_modules=('upkeep', ),
+      entry_points={
+          'console_scripts': (
+              'ecleans = upkeep:ecleans',
+              'emerges = upkeep:emerges',
+              'esync = upkeep:esync',
+              'rebuild-kernel = upkeep:rebuild_kernel_command',
+              'upgrade-kernel = upkeep:upgrade_kernel_command',
+          ),
+      },
+      test_suite='tests',
+      tests_require=('coveralls', 'pytest', 'pytest-cov', 'pytest-mock'))
