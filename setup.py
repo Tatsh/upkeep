d45af02c18f152bd2c9a3eeee241ba4428d000f4
Andrew Udvare <audvare@gmail.com>
2021-12-23 18:56:11 -0500

    setup: update

--- setup.py
+++ setup.py
@@ -1,26 +1,69 @@
 # SPDX-License-Identifier: MIT
 """Module with Portage helper commands."""
-from setuptools import setup
+import subprocess as sp
 
-setup(name='upkeep',
-      version='1.2.7',
-      author='Andrew Udvare',
-      author_email='audvare@gmail.com',
-      url='https://github.com/Tatsh/upkeep',
-      license='LICENSE.txt',
-      description='Portage update helper scripts.',
-      long_description=open('README.md').read(),
-      long_description_content_type='text/markdown',
-      py_modules=('upkeep', ),
-      python_requires='>=3.7',
-      entry_points={
-          'console_scripts': (
-              'ecleans = upkeep:ecleans',
-              'emerges = upkeep:emerges',
-              'esync = upkeep:esync',
-              'rebuild-kernel = upkeep:rebuild_kernel_command',
-              'upgrade-kernel = upkeep:upgrade_kernel_command',
-          ),
-      },
-      test_suite='tests',
-      tests_require=('coveralls', 'pytest', 'pytest-cov', 'pytest-mock'))
+from setuptools import Command, setup
+
+
+class BuildDocumentationCommand(Command):
+    """A custom command to generate documentation with Sphinx."""
+
+    description = 'Generate documentation.'
+    user_options = [('type=', 'M', 'type of documentation')]
+
+    # pylint: disable=attribute-defined-outside-init
+    def initialize_options(self) -> None:
+        self.type = 'help'
+        # pylint: enable=attribute-defined-outside-init
+
+    def finalize_options(self) -> None:
+        pass
+
+    def run(self) -> None:
+        sp.run(('sphinx-build', '-M', self.type, 'docs', 'build'), check=True)
+
+
+ENTRY_POINTS = {
+    'console_scripts': (
+        'ecleans = upkeep:ecleans',
+        'emerges = upkeep:emerges',
+        'esync = upkeep:esync',
+        'rebuild-kernel = upkeep:rebuild_kernel_command',
+        'upgrade-kernel = upkeep:upgrade_kernel_command',
+    ),
+}
+EXTRAS_REQUIRE = {
+    'dev': [
+        'mypy',
+        'mypy-extensions',
+        'pylint',
+        'rope',
+    ],
+    'docs': ['sphinx'],
+    'testing': [
+        # 'mock',
+        'pytest',
+        'pytest-cov',
+        # 'pytest-mock',
+    ]
+}
+
+with open('README.md') as f:
+    setup(
+        author='Andrew Udvare',
+        author_email='audvare@gmail.com',
+        cmdclass={'build_docs': BuildDocumentationCommand},
+        description='Portage update helper scripts.',
+        entry_points=ENTRY_POINTS,
+        extras_require=EXTRAS_REQUIRE,
+        license='LICENSE.txt',
+        long_description=f.read(),
+        long_description_content_type='text/markdown',
+        name='upkeep',
+        py_modules=['upkeep'],
+        python_requires='>=3.8',
+        test_suite='tests',
+        tests_require=('coveralls', 'pytest', 'pytest-cov', 'pytest-mock'),
+        url='https://github.com/Tatsh/upkeep',
+        version='1.2.7',
+    )
