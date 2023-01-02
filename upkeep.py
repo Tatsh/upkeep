8bec146b625f11bf2ab189969499576cc2e32112
Andrew Udvare <audvare@gmail.com>
2023-01-01 19:11:58 -0500

    add FEATURES to special env vars

--- upkeep.py
+++ upkeep.py
@@ -40,9 +40,9 @@ GRUB_CFG = '/boot/grub/grub.cfg'
 INTEL_UC = '/boot/intel-uc.img'
 KERNEL_SRC_DIR = '/usr/src/linux'
 OLD_KERNELS_DIR = '/var/lib/upkeep/old-kernels'
-SPECIAL_ENV = ('CONFIG_PROTECT', 'CONFIG_PROTECT_MASK', 'HOME', 'LANG',
-               'MAKEOPTS', 'PATH', 'SHELL', 'SSH_AGENT_PID', 'SSH_AUTH_SOCK',
-               'TERM', 'USE')
+SPECIAL_ENV = ('CONFIG_PROTECT', 'CONFIG_PROTECT_MASK', 'FEATURES', 'HOME',
+               'LANG', 'MAKEOPTS', 'PATH', 'SHELL', 'SSH_AGENT_PID',
+               'SSH_AUTH_SOCK', 'TERM', 'USE')
 # --getbinpkg=n is broken when FEATURES=getbinpkg
 # https://bugs.gentoo.org/759067
 DISABLE_GETBINPKG_ENV_DICT = dict(FEATURES='-getbinpkg')
