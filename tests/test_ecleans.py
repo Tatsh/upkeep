32b7c98096e2b2c20e65df732c773b918529dcd2
Andrew Udvare <audvare@gmail.com>
2023-01-31 03:32:15 -0500

    tests/test_ecleans: use CliRunner

--- tests/test_ecleans.py
+++ tests/test_ecleans.py
@@ -1,8 +1,7 @@
 # SPDX-License-Identifier: MIT
 from unittest.mock import patch
-import sys
 
-import pytest
+from click.testing import CliRunner
 
 from upkeep.commands.ecleans import ECLEANS_COMMANDS, ecleans
 
@@ -14,18 +13,11 @@ def test_ecleans_exception(sp_mocker: SubprocessMocker) -> None:
                           raise_=True,
                           check=True)
     with patch('upkeep.utils.sp.run', side_effect=sp_mocker.get_output):
-        sys.argv = ['ecleans']
-        with pytest.raises(SystemExit) as e:
-            ecleans()
-        assert e.value.code != 0
+        assert CliRunner().invoke(ecleans).exit_code != 0
 
 
 def test_ecleans(sp_mocker: SubprocessMocker) -> None:
     for command in ECLEANS_COMMANDS:
         sp_mocker.add_output4(command, check=True)
     with patch('upkeep.utils.sp.run', side_effect=sp_mocker.get_output):
-        sys.argv = ['ecleans']
-        try:
-            ecleans()
-        except SystemExit as e:
-            assert e.code == 0
+        assert CliRunner().invoke(ecleans).exit_code == 0
